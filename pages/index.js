9f0c8b7fe8791be12d5f30a8d40037e960189f36
Adam Chambers <adammchambers@gmail.com>
2022-04-05 20:46:07 -0400

    Install and configure Tailwind

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Lotus Gang</h1>
+      <h1 className="text-3xl font-bold underline">Lotus Gang</h1>
     </>
   )
 }
