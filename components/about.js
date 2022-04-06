bf83868b297350c338c21a90971d281435f74e89
Adam Chambers <adammchambers@gmail.com>
2022-04-06 19:23:43 -0400

    Add gang gif

--- components/about.js
+++ components/about.js
@@ -8,9 +8,11 @@ const About = () => {
       id="about"
       underline="right"
       className="pb-40 md:pb-0">
-      <div className="flex flex-col items-start max-w-2xl mt-12 md:flex-row">
-        <Image src="/img/lotus-lady-fpo.jpg" width={212} height={212} />
-        <div className="mt-6 space-y-4 text-lg leading-relaxed md:mt-0 md:ml-6">
+      <div className="flex flex-col items-start justify-start mt-12 md:max-w-4xl md:flex-row">
+        <div className="flex">
+          <img src="/img/gang.gif" style={{ maxWidth: 215 }} />
+        </div>
+        <div className="w-full mt-6 space-y-4 text-lg leading-relaxed md:mt-0 md:ml-6">
           <p>
             Lotus Gang is a brand built around art, education and empowerment.
           </p>
