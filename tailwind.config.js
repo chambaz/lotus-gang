2355440ad550d76ac62fdf7b8854db1e3973679c
Adam Chambers <adammchambers@gmail.com>
2022-04-05 23:14:56 -0400

    Add lotus beige / red to color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lotus-beige': '#fff3e1',
+        'lotus-red': '#B8513D',
+      },
+    },
   },
   plugins: [],
 }
