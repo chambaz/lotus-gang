cc1bf9ebaba195cee90461a8b51d936872def24a
Adam Chambers <adammchambers@gmail.com>
2022-04-06 00:47:16 -0400

    Style home / about section typography

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="w-screen h-full bg-lotus-beige">
         <Nav />
         <Section title="Lotus Gang">
-          <p className="max-w-lg my-8 text-lg leading-relaxed">
+          <p className="max-w-lg mt-12 mb-6 text-lg leading-relaxed">
             The Lotus is a native Indian pond flower. It is described as a burst
             of purity in muddy waters and is a symbol of wisdom.{' '}
           </p>
@@ -28,10 +28,32 @@ export default function Home() {
           </button>
         </Section>
         <Section title="About" underline="right">
-          <p className="max-w-lg my-8 text-lg leading-relaxed">
-            The Lotus is a native Indian pond flower. It is described as a burst
-            of purity in muddy waters and is a symbol of wisdom.{' '}
-          </p>
+          <div className="flex items-start mt-12">
+            <Image src="/img/lotus-lady-fpo.jpg" width={212} height={212} />
+            <div className="max-w-lg ml-6 space-y-4 text-lg leading-relaxed">
+              <p>
+                Lotus Gang is a brand built around art, education and
+                empowerment.
+              </p>
+              <p>
+                Our GEN 0 collection contains 2,000 lads and 2,000 ladies that
+                give you vaccess to our ongoing projects and closed community.
+              </p>
+
+              <p>
+                Receive exclusive insights from our research team and
+                opportunities for exposure to other exciting communities in our
+                across Blockchains.
+              </p>
+
+              <p>Lotus Gang launched on Solana during October 2021.</p>
+
+              <p>
+                We donated $52,000 USD to United Sikhs to assist with the
+                Farmer&pos;s Protest.
+              </p>
+            </div>
+          </div>
         </Section>
         <Section title="Roadmap">
           <p className="max-w-lg my-8 text-lg leading-relaxed">
