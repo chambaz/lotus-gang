ebef449656f1e601a3a6695cad25a0fcafa96873
Adam Chambers <adammchambers@gmail.com>
2022-04-06 12:19:12 -0400

    Move roadmap to array of items, adjust for small screens

--- components/roadmap.js
+++ components/roadmap.js
@@ -1,6 +1,45 @@
 import Section from './section'
 
 const Roadmap = () => {
+  const items = [
+    {
+      status: 'done',
+      text: 'Launch the first project with equal male and females',
+    },
+    {
+      status: 'done',
+      text: 'Donate 10% of Mint to Charity',
+    },
+    {
+      status: 'done',
+      text: 'Launch the first project with equal male and females',
+    },
+    {
+      status: 'done',
+      text: 'Onboard a charity to the blockchain to demonstrate the capabilities and raise awareness',
+    },
+    {
+      status: 'done',
+      text: 'Build a HUB WORLD of information and useful tools and opportunities for holders',
+    },
+    {
+      status: 'wip',
+      text: 'Explore metaverse integration in the second and third dimension',
+    },
+    {
+      status: 'wip',
+      text: 'Explore gaming and play-to-earn opportunities',
+    },
+    {
+      status: 'wip',
+      text: 'Merchandise',
+    },
+    {
+      status: 'wip',
+      text: 'Anything that creates value for holders and community',
+    },
+  ]
+
   return (
     <Section title="Roadmap">
       <div className="max-w-4xl mt-12 mb-6 text-lg leading-relaxed">
@@ -10,56 +49,19 @@ const Roadmap = () => {
           strengths.
         </p>
         <ul className="my-8">
-          <li className="flex items-center my-4">
-            <span className="w-20 px-4 py-2 mr-3 text-xs text-center text-white uppercase rounded-full font-lotus-pixelmix bg-lotus-green">
-              Done
-            </span>
-            Launch the first project with equal male and females
-          </li>
-          <li className="flex items-center my-4">
-            <span className="w-20 px-4 py-2 mr-3 text-xs text-center text-white uppercase rounded-full font-lotus-pixelmix bg-lotus-green">
-              Done
-            </span>
-            Donate 10% of Mint to Charity
-          </li>
-          <li className="flex items-center my-4">
-            <span className="w-20 px-4 py-2 mr-3 text-xs text-center text-white uppercase rounded-full font-lotus-pixelmix bg-lotus-green">
-              Done
-            </span>
-            Onboard a charity to the blockchain to demonstrate the capabilities
-            and raise awareness
-          </li>
-          <li className="flex items-center my-4">
-            <span className="w-20 px-4 py-2 mr-3 text-xs text-center text-white uppercase rounded-full font-lotus-pixelmix bg-lotus-green">
-              Done
-            </span>
-            Build a HUB WORLD of information and useful tools and opportunities
-            for holders
-          </li>
-          <li className="flex items-center my-4">
-            <span className="w-20 px-4 py-2 mr-3 text-xs text-center text-white uppercase rounded-full font-lotus-pixelmix bg-lotus-orange">
-              wip
-            </span>
-            Explore metaverse integration in the second and third dimension
-          </li>
-          <li className="flex items-center my-4">
-            <span className="w-20 px-4 py-2 mr-3 text-xs text-center text-white uppercase rounded-full font-lotus-pixelmix bg-lotus-orange">
-              wip
-            </span>
-            Explore gaming and play-to-earn opportunities
-          </li>
-          <li className="flex items-center my-4">
-            <span className="w-20 px-4 py-2 mr-3 text-xs text-center text-white uppercase rounded-full font-lotus-pixelmix bg-lotus-orange">
-              wip
-            </span>
-            Merchandise
-          </li>
-          <li className="flex items-center my-4">
-            <span className="w-20 px-4 py-2 mr-3 text-xs text-center text-white uppercase rounded-full font-lotus-pixelmix bg-lotus-orange">
-              wip
-            </span>
-            Anything that creates value for holders and community
-          </li>
+          {items.map((item) => {
+            const classes =
+              item.status === 'done' ? 'bg-lotus-green' : 'bg-lotus-orange'
+            return (
+              <li className="flex items-start my-6">
+                <span
+                  className={`w-20 px-4 py-2 mr-3 text-xs text-center text-white uppercase rounded-full font-lotus-pixelmix ${classes}`}>
+                  {item.status}
+                </span>
+                <p className="w-full ml-auto">{item.text}</p>
+              </li>
+            )
+          })}
         </ul>
         <p>
           With the speed at which this space changes, we aim to adapt and
