e978ed650c4044e017397f4de5edaa1034bca407
Adam Chambers <adammchambers@gmail.com>
2022-04-06 03:28:28 -0400

    Uppercase section titles

--- components/section.js
+++ components/section.js
@@ -4,7 +4,7 @@ const Section = ({ children, title, underline = 'left' }) => {
     <div className="flex items-center w-screen min-h-screen">
       <div className="container px-8 mx-auto">
         <h1
-          className={`${underlinePos} inline text-6xl font-lotus-pixelmix-bold relative after:content-[''] after:block after:absolute after:w-screen after:h-2 after:mt-4 after:bg-lotus-red`}>
+          className={`${underlinePos} inline uppercase text-6xl font-lotus-pixelmix-bold relative after:content-[''] after:block after:absolute after:w-screen after:h-2 after:mt-4 after:bg-lotus-red`}>
           {title}
         </h1>
         {children}
